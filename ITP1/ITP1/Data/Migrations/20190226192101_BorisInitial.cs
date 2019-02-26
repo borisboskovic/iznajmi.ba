48fdf186210b991f82c0d1f931856fa54c9e0021
Boris Boskovic <boris.boskovic92@gmail.com>
2019-02-26 21:33:28 +0100

    Promijenjen prikaz avatara Korisnika (Edit)

---
+++ ITP1/ITP1/Data/Migrations/20190226192101_BorisInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ITP1.Data.Migrations
+{
+    public partial class BorisInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
